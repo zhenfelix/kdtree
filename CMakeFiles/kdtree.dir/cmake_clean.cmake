b761a1b655a994ca4c1622b71822c659bf305d7e
sherlock <you@example.com>
2025-10-20 15:20:27 +0800

    debug

---
+++ CMakeFiles/kdtree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kdtree.dir/kdtree.c.o"
+  "CMakeFiles/kdtree.dir/kdtree.c.o.d"
+  "CMakeFiles/kdtree.dir/sample.cpp.o"
+  "CMakeFiles/kdtree.dir/sample.cpp.o.d"
+  "kdtree"
+  "kdtree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/kdtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
